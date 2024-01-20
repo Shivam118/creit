c1a9c18c157810030de090007361a5ad7684524d
Shivam118 <sharmashushant118@gmail.com>
2024-01-20 13:09:49 +0530

    Half away

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,89 @@
+
 import Image from 'next/image'
+import { CiCreditCard1 } from "react-icons/ci";
+import { GoProjectRoadmap, GoQuestion } from "react-icons/go";
+
+import "./App.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="bodyContainer">
+      <main className="main-container">
+        <nav className="header">
+          <div className='headerLogo'>
+            <a href="#">CreIT</a>
+          </div>
+          <ul className='navList'>
+            <li className='navItem'><a href="#" className='navLinks'><span><GoProjectRoadmap /></span>  Benefits</a></li>
+            <li className='navItem'><a href="#" className='navLinks'><span><CiCreditCard1 /></span>  Pricing</a></li>
+            <li className='navItem'><a href="#" className='navLinks'><span><GoQuestion /></span>  FAQs</a></li>
+          </ul>
+        </nav>
+        <section className='hero'>
+          <h1>The better way to get Development Done</h1>
+          <p>Development as a Service for startups, scaleups, enterprises and agencies. Just join, create some asks, and we deliver.</p>
+          <button>See plans</button>
+        </section>
+        <section className='benefits'>
+          <div className="engineerWorks">
+            <h3>Trusted By teams at</h3>
+            <div className="companiesLogo">
+              {/* <Image src="/images/creit-engineer-works.png" alt="CreIT Engineer Works" width={100} height={100} /> */}
+            </div>
+          </div>
+          <div className='techStack'>
+            <h2>Variety of capabilities from the top 1% of engineering talent around the world ✨</h2>
+            <div className='techStackCards'>
+              <span>React</span>
+              <span>Vue</span>
+              <span>Angular</span>
+              <span>Next.Js</span>
+              <span>Nest.Js</span>
+              <span>Node.Js</span>
+              <span>Express</span>
+              <span>Django</span>
+              <span>React Native</span>
+              <span>Flutter</span>
+              <span>Swift</span>
+              <span>Kotlin</span>
+              <span>AWS</span>
+              <span>Azure</span>
+              <span>GCP</span>
+              <span>Kubernetes</span>
+              <span>Docker</span>
+              <span>UI/UX</span>
+              <span>Figma</span>
+              <span>Sketch</span>
+              <span>Adobe XD</span>
+              <span>Selenium</span>
+              <span>Cypress</span>
+              <span>Solidity</span>
+              <span>Web3.js</span>
+              <span>TensorFlow</span>
+              <span>PyTorch</span>
+              <span>Keras</span>
+            </div>
+          </div>
+        </section>
+        <section className='pricing'>
+          <h3 className='pricingHeader'>Membership levels</h3>
+          <p className='pricingDescription'>We offer a simple pricing structure that is easy to understand and easy to budget for.</p>
+          <div className='pricingCards'>
+            <div className='pricingCard'>
+            </div>
+          </div>
+        </section>
+        <section className='testimonials'></section>
+        <section className='faq'></section>
+        <section className='extraCard'>
+          <h1>See if we are right for you.</h1>
+          <p>Get a guided tour through creIT Labs, and find out how you and your team can benefit from having a full stack development subscription.</p>
+          <button>Get Started</button>
+        </section>
+        <footer className='footer'>
+          Copyright © 2023. All rights reserved.
+        </footer>
+      </main>
+    </div>
   )
 }
